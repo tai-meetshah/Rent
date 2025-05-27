2984240cec868e69da730d892aa7a065ca0cc2f4
tai-meetshah <theappideas.meet@gmail.com>
2025-05-27 17:58:17 +0530

    Refactor user management:

--- controllers/admin/userController.js
+++ controllers/admin/userController.js
@@ -1,32 +1,32 @@
-const User = require("../../models/userModel");
+const User = require('../../models/userModel');
 
 exports.getAllUsers = async (req, res) => {
-
     try {
-        const users = await User.find({isDelete: false}).select('qrCode name mobileNumber birthDate gender createdAt isActive').sort('-_id');
- 
+        const users = await User.find({ isDelete: false })
+            .select('name email city createdAt isActive')
+            .sort('-_id');
+
         res.render('user', { users });
     } catch (error) {
         req.flash('red', error.message);
         res.redirect('/');
     }
-
 };
 
 exports.viewUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      req.flash("red", "User not found!");
-      return res.redirect("/user");
-    }
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            req.flash('red', 'User not found!');
+            return res.redirect('/user');
+        }
 
-    res.render("user_view", { user });
-  } catch (error) {
-    if (error.name === "CastError") req.flash("red", "User not found!");
-    else req.flash("red", error.message);
-    res.redirect("/user");
-  }
+        res.render('user_view', { user });
+    } catch (error) {
+        if (error.name === 'CastError') req.flash('red', 'User not found!');
+        else req.flash('red', error.message);
+        res.redirect('/user');
+    }
 };
 
 exports.changeUserStatus = async (req, res) => {
@@ -38,7 +38,7 @@ exports.changeUserStatus = async (req, res) => {
             return res.redirect('/user');
         }
 
-        user.isActive = req.params.status
+        user.isActive = req.params.status;
 
         await user.save();
 
